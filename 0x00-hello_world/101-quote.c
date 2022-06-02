f68e5de2e99754af8d0b050978c6fb0778bca6f4
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-02 16:08:05 -0700

    fix: issues

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
 * main -entry function
-* Return 1
+* Return: 1
 */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2,str,sizeof(str));
+	write(2, str, sizeof(str));
 	return (1);
 }
