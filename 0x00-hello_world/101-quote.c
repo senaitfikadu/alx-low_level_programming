8450e821eec5bea7df86617e958ac4927071c4bb
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-02 16:30:00 -0700

    fix: issues

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
 */
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(1, str, sizeof(str));
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
