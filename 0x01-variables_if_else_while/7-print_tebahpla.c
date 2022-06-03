d122f5480b91b3bf935bfee331ec9d7349b123a8
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-03 16:18:22 -0700

    commit reverse alphabet lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - prints lowecase alphabet in reverse
+* Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for ( = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
