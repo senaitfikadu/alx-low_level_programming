b0f36d57518f68b3a268970ab4c2dd88f56d9dd2
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-03 12:27:55 -0700

    add last string number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
- 	b = n % 10;
+	b = n % 10;
 
 	if (b > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, b);
