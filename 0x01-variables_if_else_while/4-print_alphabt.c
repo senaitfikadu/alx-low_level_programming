07935c5c905a88bc0a6cb38dc8a0e0e46aade610
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-03 15:03:04 -0700

    add print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
-
+/**
+ * main - assaign for loop
+ * Return: 0
+ */
 int main(void)
 {
 	int c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if(c !='q' && c != 'e')
+		if (c != 'q' && c != 'e')
 		putchar(c);
 	}
 	return (0);
