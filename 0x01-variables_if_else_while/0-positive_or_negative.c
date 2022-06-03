eca3a1e9b8d58e8a69b211f513b9fad1cb1587de
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-03 08:07:48 -0700

    add if else function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,16 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - assigns a random number to int n everytime
-* it executes, and prints it
-* Return: Always 0 (Success) 
+* main - if else function
+* Return: 0
 */
 int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND - MAX / 2;
 	if (n > 0)
 		printf("%d is posetive/n", n);
 	else if (n < 0)
