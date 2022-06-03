c4bbf18ed0042a8c028bf0afeb55c994c4885f27
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-03 16:04:14 -0700

    add number z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers of base
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
