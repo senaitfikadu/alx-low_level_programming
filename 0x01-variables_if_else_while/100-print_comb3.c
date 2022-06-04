358c8a2cca02ad3c9ed742cef785c08c1358ed8b
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-04 08:55:56 -0700

    add differen commpaination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
-* main - uses while loop
-*
-* Return: 0
-*/
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
 int main(void)
 {
 	int e, i;
@@ -21,7 +31,8 @@ int main(void)
 			{
 				putchar(e);
 				putchar(i);
-				if (e == 57 && i == 56)
+
+		if (e == 57 && i == 56)
 				{
 				break;
 				}
