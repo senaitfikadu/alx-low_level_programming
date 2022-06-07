1aec65198fc66ba587429ecc21edea7f5cde048d
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-07 08:33:52 -0700

    prints_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints putchar
@@ -14,7 +14,7 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{	
-			_putchar(text[i]);
+		_putchar(text[i]);
 	}
 	_putchar('\n');
 
