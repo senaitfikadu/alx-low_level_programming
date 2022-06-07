33f7ba58d0e3b366a1685ac13c1be4a47faa48fc
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-07 14:19:15 -0700

    10xalphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet_x10
- *
- * Return: always 0
- */
+* print_alphabet_x10 - print lowercase alphabet x 10
+*
+* Return: always 0
+*/
 void print_alphabet_x10(void)
 {
 	char i, e;
 
 	for (i = 0; i < 10; i++)
 	{
-	for (e = 'a' ; e <= 'z'; e++){
-			
+	for (e = 'a' ; e <= 'z'; e++)
+	{
 		_putchar(e);
 	}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
