c16485167014898a98f36f0da8a1defbdfd82a72
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-07 14:57:29 -0700

    print islower lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+* _islower - print islower
+* 
+* Return: always 0
+*/
+int _islower(int c)
+{
+	int islower(int c);
+
+	if (islower(c))
+	{
+		_putchar("'%c' is lowercase\n", letter);
+	}
+	 else
+	{
+		_putchar("'%c' is uppercase\n", letter);
+}
+		 
+
+
+
