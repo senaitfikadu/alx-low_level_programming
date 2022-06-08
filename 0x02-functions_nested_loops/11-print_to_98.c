e7a370364d013c05cefacb04e896f69559877630
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-07 17:57:25 -0700

    add loop for 98 iteration

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@ void print_to_98(int n)
 {
 	if (n==98)
 	{
-		printf("%d ",98);
+		printf("%d",98);
 	}
 	else if (n>98)
 	{
@@ -25,5 +25,6 @@ void print_to_98(int n)
 			n++;
 		}
 	}
+	printf("\n");
 		
 }
