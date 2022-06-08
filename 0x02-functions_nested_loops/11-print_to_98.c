6453d07f2090ce35c2cdaa23c10a9565f9c76f98
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-07 17:36:19 -0700

    add loop for 98 iteration

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
-void print_to_98(int n){
-	if (n>98){
-		while(n>=98){
+void print_to_98(int n)
+{
+	if (n>98)
+	{
+		while(n>=98)
+		{
 			_putchar(n);
 			_putchar(',');
 			n--;
 		}
 	}
-	else{
-		while(n<=98){
+	else
+	{
+		while(n<=98)
+		{
 			_putchar(n);
 			_putchar(',');
 			n++;
 		}
-	}
 	}
 		
 }
