0a8a81ba7a05c2472ec1600eeaa07090273c9e8c
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-24 13:19:38 -0700

    0 commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,13 @@
 incude "main.h"
-include <stdio.h>
+include < stdio.h >
 
 /**
- *main - prints name of the program
- *
- *@argc:arguement count
- *@argv:arguement variable
- *
- *Return:0(success)
- *
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
