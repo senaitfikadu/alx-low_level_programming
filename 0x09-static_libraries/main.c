381bb898c0f89e68ece34028aafd92cebf1cb540
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-27 12:07:28 -0700

    add c library

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
 int main(void)
 {
-    printf("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+printf("\"At the end of the day,
+	my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
