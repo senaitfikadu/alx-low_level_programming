ab1e34c4d4414fdfce056207db38dc0872b06e00
senaitfikadu <senaitfikadu44@gmail.com>
2022-06-29 12:43:50 -0700

    100-argstostr

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,3 +4,4 @@ char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
