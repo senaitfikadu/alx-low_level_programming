33254cddfd6a76f535d4d427959eeb07f9170f32
senaitfikadu <senaitfikadu44@gmail.com>
2022-07-07 14:07:40 -0700

    the three commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,71 +1,62 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- *op_add - adds two int
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
  *
- *@a:int param
- *@b:int param
- *Return:int
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- *op_sub - returns diff of a and b
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
  *
- *@a:int param
- *@b:int param
- *
- *Return:difference
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- *op_mul - returns product of a and b
- *@a:int
- *@b:int
+ * op_mul - calculates the product of two integers
+ * @a: first integer
+ * @b: second integer
  *
- *Return:product
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- *op_div - returns result of div of a and b
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
  *
- *@a:int
-  *@b:int
- *
- *Return:div
+ * Return: result of the division of a and b
  */
 int op_div(int a, int b)
 {
-	if (!b)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
+
 /**
- *op_mod - returns modulus of a and b
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
  *
- *@a:int
- *@b:int
- *
- *Return: modulus
+ * Return: remainder of a divided b
  */
 int op_mod(int a, int b)
 {
-	if (!b)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
-#ifndef function_pointers
-#define function_pointers
-int _putchar(char c);
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
 
 #endif
