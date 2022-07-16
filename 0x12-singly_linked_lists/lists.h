d348a48763a541d6428a44af96400a8066276be1
senaitfikadu <senaitfikadu44@gmail.com>
2022-07-16 08:58:09 -0700

    fix: betty linter

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,7 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,11 +16,8 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
+} list_t;
 
-}list_t;
-/**
- * int _putchar(char c);
- */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
