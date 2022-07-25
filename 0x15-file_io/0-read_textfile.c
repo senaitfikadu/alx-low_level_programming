11b022521f981a729500ccfc12f4dc01d37cad30
senaitfikadu <senaitfikadu44@gmail.com>
2022-07-25 06:25:31 -0700

    update the read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,14 +9,14 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fb;
+	int fd;
 	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
