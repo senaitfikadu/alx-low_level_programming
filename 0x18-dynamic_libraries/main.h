e5b9e4ceedbd8da58d0af6af13c72aa977f90be1
senaitfikadu <senaitfikadu44@gmail.com>
2022-09-09 09:28:33 -0700

    dynamic liberaries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
-#ifndef __MAIN__
-#define __MAIN__
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -15,13 +18,12 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int nrandom();
 
-
-#endif /* __MAIN__ */
+#endif /* MAIN_H */
